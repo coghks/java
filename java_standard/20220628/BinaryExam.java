744d16cfd640783f5a2d23af17a3ba33bb0b424c
Hwani <86889594+coghks@users.noreply.github.com>
2023-06-28 23:25:03 +0900

    Update BinaryExam.java

--- java_standard/20220628/BinaryExam.java
+++ java_standard/20220628/BinaryExam.java
@@ -1,11 +1,11 @@
 public class BinaryExam {
     public static void main(String[] args) {
         int data = 123;
-        String binary = java.lang.Integer.toBinaryString(data);
+        String binary = java.lang.Integer.toBinaryString(data);     //2진수로 바꾸기
         System.out.println("binary = " + binary);
-        String octal = java.lang.Integer.toOctalString(data);
+        String octal = java.lang.Integer.toOctalString(data);       //8진수로 바꾸기
         System.out.println("octal = " + octal);
-        String hexa = java.lang.Integer.toHexString(data);
+        String hexa = java.lang.Integer.toHexString(data);          //16진수로 바꾸기
         System.out.println("hexa = " + hexa);
         
         int x = 123;
