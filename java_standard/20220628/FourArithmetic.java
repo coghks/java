c7e8db408bf71d6abed3e61556261fa58a5c164d
Hwani <86889594+coghks@users.noreply.github.com>
2023-06-28 23:27:18 +0900

    Update FourArithmetic.java

--- java_standard/20220628/FourArithmetic.java
+++ java_standard/20220628/FourArithmetic.java
@@ -1,6 +1,6 @@
 public class FourArithmetic {
     public static void main(String[] args){
-      int a,b;
+      int a,b; //4칙연산 계산기
       a=12;
       b=2;
         System.out.println("a+b="+(a+b));
