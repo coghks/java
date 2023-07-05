102028fcfd622499ba7f11244581ea64ca129364
Hwani <86889594+coghks@users.noreply.github.com>
2023-07-06 00:41:38 +0900

    Add files via upload

---
+++ java_codingtest/greedy/greedy2r.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class greedy2r {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int result=0;
+        for(int i=0;i<n;i++){
+            int minv=10001;//10000이 최대이므로
+            for(int j=0;j<m;j++){
+                int x=sc.nextInt();
+                minv=Math.min(x,minv);
+
+            }
+            result=Math.max(minv,result);
+        }
+        System.out.println(result);
+    }
+}
