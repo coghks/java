7945735d0d94dd1ecc452ef73b0f5ad9febd3f51
Hwani <86889594+coghks@users.noreply.github.com>
2023-07-13 00:05:01 +0900

    Update gu_hyeon1.java

--- java_codingtest/gu_hyeon/gu_hyeon1.java
+++ java_codingtest/gu_hyeon/gu_hyeon1.java
@@ -11,11 +11,11 @@ public class gu_hyeon1 {
 
         int[] dx={0,0,-1,1};
         int[] dy={-1,1,0,0};
-        char[] move_types={'L','R','U','D'};
+        char[] move_types={'L','R','U','D'}; //String 참조형 배열이 아닌 기본형 배열로 받아야 값을 비교할 수 있따. 
 
 
         for (int i=0;i<plans.length;i++){
-            char plan=plans[i].charAt(0);
+            char plan=plans[i].charAt(0); //참조형 배열에는 값 자체가 아닌 주소가 저장되어 있으므로 비교를 위해선 char로 바꿔줘야함
             for (int j=0;j< move_types.length;j++){
                 if(move_types[j]==plan){
                     nx=x+dx[j];
