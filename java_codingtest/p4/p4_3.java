29d7977b334aecb5810c64d6c8e4aa8da38f10db
Hwani <86889594+coghks@users.noreply.github.com>
2023-08-05 01:51:56 +0900

    Update p4_3.java

--- java_codingtest/p4/p4_3.java
+++ java_codingtest/p4/p4_3.java
@@ -4,7 +4,7 @@ class Solution {
         StringBuilder sBuilder = new StringBuilder();
         int count = 0;
         for (int i=0; i<s.length(); i++){
-            if (s.charAt(i)>'A' && s.charAt(i)<'z'){
+            if (s.charAt(i)>='A' && s.charAt(i)<='z'){ //문제의 요놈때문에 A와 z가 포함되면 테스트 실패했었음
                 if (count % 2 == 0 && (int)s.charAt(i)>90){
                     sBuilder.append((char)(s.charAt(i)-32));
                     count++;
@@ -36,7 +36,7 @@ class Solution {
         return answer;
     }
 }
-//프로그래머스 12930번(테스트케이스 몇개는 성공 몇개는 실패하는데 이유를 모르겠음..)
+//프로그래머스 12930번(테스트케이스 몇개는 성공 몇개는 실패하는데 이유를 모르겠음..) --> A와 z를 빼먹었음!
 //1.문자열 s를 받는다.
 //2.공백을 기준으로 각 단어의 짝수 번째 알파벳은 대문자로 홀수 번째 알파벳은 소문자로 출력
 //3.count를 만들어 각자리수가 짝수인지 홀수인지 판별 공백이 나오게 되면 count는 0으로 초기화
