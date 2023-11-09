909d99c58c90a28d83894355f751fb2014574f52
Hwani <86889594+coghks@users.noreply.github.com>
2023-11-09 21:01:20 +0900

    Create p4_4.java

---
+++ java_codingtest/p4/p4_4.java
@@ -0,0 +1,18 @@
+class Solution{
+    public int solution(int n){
+        String str = Integer.toString(n,3);
+        String reversed = new StringBuilder(str).reverse().toString();
+        return Integer.valueOf(reversed,3);
+
+    }
+}
+
+//다른풀이
+class Solution{
+    public int solution(int n){
+        String str = Integer.toString(n,3);
+        String reversed = new StringBuilder(str).reverse().toString();
+        return Integer.parseInt(reversed,3);
+    
+    }
+}
