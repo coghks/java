f42848e575dbe889a04107635ed78ba94e71e612
Hwani <86889594+coghks@users.noreply.github.com>
2023-11-15 20:13:04 +0900

    Create p4_9.java

---
+++ java_codingtest/p4/p4_9.java
@@ -0,0 +1,27 @@
+public class Solution{
+    public String solution(String newId){
+        // 1단계
+        newId = newId.toLowerCase();
+        // 2단계
+        newId = newId.replaceAll("[^a-z0-9\\-_.]","");
+        // 3단계
+        newId = newId.replaceAll("\\.+",".");
+        // 4단계
+        newId = newId.replaceAll("^\\.+|\\.+$","");
+        // 5단계
+        if (newId.isEmpty()) newId = "a";
+        // 6단계
+        if (newId.length() >= 16){
+            newId = newId.substring(0,15);
+            newId = newId.replaceAll("\\.+$","");
+        }
+        // 7단계
+        while (newId.length() < 3){
+            newId += newId.charAt(newId.length() - 1);
+        }
+        return newId;
+    }
+}
+
+
+https://school.programmers.co.kr/learn/courses/30/lessons/72410
